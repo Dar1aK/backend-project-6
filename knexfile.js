5fc8ebe4c7b5d49aca0e4c93a404bc0cbeaead6b
Daria Karataeva <daria.karataeva@yandex.ru>
2024-12-07 17:22:00 +0300

    fix dotenv

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import 'dotenv/config.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
