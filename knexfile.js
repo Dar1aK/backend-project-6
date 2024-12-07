d7e021b0e9feb198efb009928d69c11e581e8b5e
Daria Karataeva <daria.karataeva@yandex.ru>
2024-12-07 17:05:00 +0300

    fix pg connection1

--- knexfile.js
+++ knexfile.js
@@ -25,18 +25,20 @@ export const test = {
   migrations,
 };
 
+console.log('process.env***', process.env)
+
 export const production = {
   client: 'pg',
-  connection: process.env.DB_URL,
-  searchPath: ['knex', 'public'],
-  // connection: {
-  //   connectionString: process.env.DB_URL,
-  //   host: process.env.DB_HOST,
-  //   port: process.env.DB_PORT,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME,
-  // },
+  // connection: process.env.DB_URL,
+  // searchPath: ['knex', 'public'],
+  connection: {
+    connectionString: process.env.DB_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
   pool: { min: 0, max: 200 },
   useNullAsDefault: true,
   migrations,
